54feafc34c7c801e866a702673913733846807bd
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-06-22 14:53:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 #import mysql.connector
-from connection_test import check_mysql_connection
+from connection_test import check_mysql_connection,fetch_press_releases
 import os
 from dotenv import load_dotenv
 import uvicorn
@@ -13,7 +13,27 @@ host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")
 
 
-@app.get("/")
+@app.get("/{user_id}")
+async def root(user_id: str):
+    connection =check_mysql_connection()
+    cursor = connection.cursor(dictionary=True)
+    user_session_id = user_id
+    all_release = fetch_press_releases(user_session_id)
+    release = all_release[-1]
+
+    # Prepare the Arabic prompt
+    print(release['about_press'])
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return {"Press release":release['about_press']}
+
+if __name__ == "__main__":              
+    uvicorn.run(app, host=host, port=port)
+
+'''@app.get("/")
 async def root():
     connection = check_mysql_connection()
     '''cursor = connection.cursor(dictionary=True)
@@ -28,7 +48,7 @@ async def root():
     cursor.close()
     connection.close()'''
 
-    return {"connection":connection}
+    return {"connection":"done"}
 
 if __name__ == "__main__":              
-    uvicorn.run(app, host=host, port=port)
+    uvicorn.run(app, host=host, port=port)'''
