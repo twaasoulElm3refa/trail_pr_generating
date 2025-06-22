d42099cfab712a24b39671ce67758f23eb443666
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-06-22 14:55:24 +0300

    Update connection_test.py

--- connection_test.py
+++ connection_test.py
@@ -22,5 +22,32 @@ def check_mysql_connection():
 
     except Error as e:
         print("❌ Error:", e)
+        
+def fetch_press_releases(user_id: str ):
+    connection = get_db_connection()
+    if connection is None:
+        print("Failed to establish database connection")
+        return []
+    
+    try:
+        cursor = connection.cursor(dictionary=True)
+        query = """
+        SELECT * 
+        FROM wpl3_press_release_Form
+        WHERE user_id = %s 
+        """
+        cursor.execute(query, (user_id,))
 
+        # Fetch the first row
+        all_user_articles = cursor.fetchall()
+
+        return all_user_articles
+
+    except Error as e:
+        print(f"Error fetching data: {e}")
+        return []
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
 #check_mysql_connection()
