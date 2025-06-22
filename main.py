250be4043c59fbdd965e716ea3463d660ba2beab
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-06-22 15:40:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-import mysql.connector
+#import mysql.connector
 from connection_test import check_mysql_connection,fetch_press_releases
 import os
 from dotenv import load_dotenv
@@ -16,7 +16,7 @@ port = os.getenv("DB_PORT")
 @app.get("/{user_id}")
 async def root(user_id: str):
     connection =check_mysql_connection()
-    cursor = connection.cursor(dictionary=True)
+    #cursor = connection.cursor(dictionary=True)
     user_session_id = user_id
     all_release = fetch_press_releases(user_session_id)
     release = all_release[-1]
@@ -25,7 +25,7 @@ async def root(user_id: str):
     print(release['about_press'])
 
     connection.commit()
-    cursor.close()
+    #cursor.close()
     connection.close()
 
     return {"Press release":release['about_press']}
