74476355f10a53491fc6ef564c755a957cd46f73
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-06-24 16:36:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI
+#import mysql.connector
+from connection_test import check_mysql_connection,fetch_press_releases ,update_press_release
+from dotenv import load_dotenv
+import uvicorn
+import asyncio
+from pydantic import BaseModel
+import openai
+import faiss
+import numpy as np
+import json
+from sentence_transformers import SentenceTransformer
+
+app = FastAPI()
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+
+def generate_article_based_on_topic(topic, corpus, index):
+
+    model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+    # Find relevant documents based on the topic embedding
+    topic_embedding = model.encode([topic])
+    D, I = index.search(np.array(topic_embedding), 3)  # Get top 3 related documents
+
+    # Retrieve the content for those documents
+    context = "\n".join([corpus[i]["content"] for i in I[0]])
+
+    # Create the prompt for GPT
+    prompt = f"""
+أنت صحفي عربى محترف في مؤسسة إعلامية بارزة، ومتخصص في كتابة البيانات الإخبارية بلغة عربية فصيحة مع الالتزام بالبيانات والتفاصيل الممنوحة اليك وصياغتها فى صوره بيان مع الالتزام بعدد الاسطر 7 اسطر معتمدا فى البيان على البيانات المدخله لك من المستخدم او مواقع رسميه ذات مصادر موثقه مائة باللمائة مع ذكر فى بدايه البيان العنوان الرئيسي و تاريخ اليوم حسب الوطن العربي ثم محتوى البيان ثم كلمة 'للمحررين'  ثم الحول ثم فى نهايه البيان بيانات التواصل من تليفون و ايميل و وسائل التواصل الاجتماعى (ان ذكرت فى التلقين او البيانات المدخله من المستخدم) دون تاليف او تعديل مع ترك مساحتها فارغه اذا لم يتم تحديدها من المستخدم: {topic}.
+    استخدم المعلومات التالية كنموذج لكيقية صياغه المبيان :
+    {context}
+    و الرجوع الى موقعهم الموجود فى   لكتابه حول عنهم من خدمات يقدموها الى من هم 
+    """
+
+    # Get response from OpenAI
+    response  = openai.chat.completions.create(
+      model="gpt-4o-mini",
+      store=True,
+      messages=[{"role": "user", "content": prompt}]
+        )
+    
+    return response.choices[0].message.content.strip()
+
+@app.get("/{user_id}")
+async def root(user_id: str):
+ with open('filtered_corpus.json', 'r', encoding='utf-8') as json_file:
+            corpus = json.load(json_file)
+        index = faiss.read_index("my_index.index")
+
+        # Prepare the Arabic prompt
+        topic = f" اكتب بيان عن احداث الحرب الحالية"
+        article = generate_article_based_on_topic(topic, corpus, index)
+        
+        print(article)
+
+    return {"article":article }
+
+if __name__ == "__main__":              
+    uvicorn.run(app, host=host, port=port)
