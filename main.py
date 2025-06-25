26abb25b34a4104fc68197705fc55dd4ffa91c8f
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-06-25 11:57:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
 from fastapi import FastAPI
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+import openai
+import json
+
+app = FastAPI()
+
+model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+with open('filtered_corpus.json', 'r', encoding='utf-8') as json_file:
+    corpus = json.load(json_file)
+index = faiss.read_index("my_index.index")
+
+def generate_article(topic):
+    topic_embedding = model.encode([topic])
+    D, I = index.search(np.array(topic_embedding), 3)
+    context = "\n".join([corpus[i]["content"] for i in I[0]])
+
+    prompt = f"""
+أنت صحفي عربى محترف في مؤسسة إعلامية بارزة، ومتخصص في كتابة البيانات الإخبارية بلغة عربية فصيحة مع الالتزام بالبيانات والتفاصيل الممنوحة اليك وصياغتها فى صوره بيان مع الالتزام بعدد الاسطر 7 اسطر معتمدا فى البيان على البيانات المدخله لك من المستخدم او مواقع رسميه ذات مصادر موثقه مائة باللمائة مع ذكر فى بدايه البيان العنوان الرئيسي و تاريخ اليوم حسب الوطن العربي ثم محتوى البيان ثم كلمة 'للمحررين'  ثم الحول ثم فى نهايه البيان بيانات التواصل من تليفون و ايميل و وسائل التواصل الاجتماعى (ان ذكرت فى التلقين او البيانات المدخله من المستخدم) دون تاليف او تعديل مع ترك مساحتها فارغه اذا لم يتم تحديدها من المستخدم: {topic}.
+    استخدم المعلومات التالية كنموذج لكيقية صياغه المبيان :
+    {context}
+    و الرجوع الى موقعهم الموجود فى   لكتابه حول عنهم من خدمات يقدموها الى من هم 
+    """
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+
+@app.post("/{used_id}")
+async def root(user_id: str):
+    # Prepare the Arabic prompt
+    topic = f" اكتب بيان عن احداث الحرب الحالية"
+    article = generate_article_based_on_topic(topic)
+    
+    print(article)
+    return {"article":article }
+# return {"result": generate_article(data["topic"], data["lines_number"], data["website"])}
+
+
+
+'''from fastapi import FastAPI
 #import mysql.connector
 from connection_test import check_mysql_connection,fetch_press_releases ,update_press_release
 from dotenv import load_dotenv
@@ -29,6 +72,7 @@ def generate_article_based_on_topic(topic, corpus, index):
 
     # Retrieve the content for those documents
     context = "\n".join([corpus[i]["content"] for i in I[0]])
+    
 
     # Create the prompt for GPT
     prompt = f"""
@@ -59,7 +103,7 @@ async def root(user_id: str):
     
     print(article)
     
-    return {"article":article }
+    return {"article":article }'''
 
 if __name__ == "__main__":              
     uvicorn.run(app, host=host, port=port)
